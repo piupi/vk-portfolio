06ee0a7ae7cb5c32b1e446f4070e335d3008dd05
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2020-05-20 23:08:05 -0500

    Add icon, buttons, and filler text.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ function App() {
   return (
     <div className="card">
       <div className="about">
+      <img className="photo" src="pfp3.jpg" alt="frog" />
         <div className="name">Vanessa K.</div>
         <div className="title">Friend of cats</div>
         <span className="line"></span>
-        <div className="bio">Blah blah blah blah bacon ipsum dolor amet shank ground round ham hock buffalo ribeye. Ham hock short ribs andouille flank tri-tip beef, porchetta pancetta filet mignon corned beef pork belly. Meatball jerky porchetta spare ribs filet mignon, beef ribs shank alcatra shankle t-bone pig turkey tongue. Tail brisket tenderloin chicken, prosciutto sausage rump short ribs.</div>
+        <div className="bio">
+          Blah blah blah blah bacon ipsum dolor amet shank ground round ham hock buffalo ribeye. Ham hock short ribs andouille flank tri-tip beef, porchetta pancetta filet mignon corned beef pork belly.
+        </div>
       </div>
+      <nav>
+        <button className="current">about</button>
+        <button>projects</button>
+        <button>resume</button>
+        <button>contact</button>
+      </nav>
     </div>
   );
 }
+
+// Nvm I want to divide it into header + nav and the only part that changes is middle
 
 export default App;
