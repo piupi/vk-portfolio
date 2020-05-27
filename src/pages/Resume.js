b111fcab20faf65b7909bf6beb377ef2c6110ee4
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2020-05-27 15:28:40 -0500

    Add iframe for resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,6 +4,7 @@ const Resume = () => {
     return (
         <div>
             <h1>Resume page</h1>
+            <iframe title="Resume" src="path/to/pdf" width="500" height="700"></iframe>
         </div>
     )
 };
