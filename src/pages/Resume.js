5a21d2dfdfb3313968b5bff26fdc5c80a5eacca6
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2020-06-09 21:13:49 -0500

    Add second navbar type, change nav colors, add fonts.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Resume = () => {
     return (
         <div>
-            <h1>Resume page</h1>
-            <iframe title="Resume" src="path/to/pdf" width="500" height="700"></iframe>
+            <nav className="top-nav">
+                <div className="top-nav-items">
+                    <Link to="/">
+                        <button className="current">about</button>
+                    </Link>
+                    <Link to="/projects">
+                        <button>projects</button>
+                    </Link>
+                    <Link to="/resume">
+                        <button>resume</button>
+                    </Link>
+                    <Link to="/contact">
+                        <button>contact</button>
+                    </Link>
+                </div>
+            </nav>
+            <div className="resume">
+                <iframe title="Resume" src="public\resumemay2020.pdf" width="500" height="600"></iframe>
+            </div>
         </div>
     )
 };
