1b9aea32531f8c4a41b6bf5e3ccbccbe646f9197
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2020-06-09 22:37:16 -0500

    Embed resume with Google Drive

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import { Document, Page } from "react-pdf";
+import { Document, Page } from "react-pdf/dist/entry.webpack";
+// import pdfjs from "pdfjs-dist";
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 
 const Resume = () => {
     return (
@@ -21,10 +27,15 @@ const Resume = () => {
                 </div>
             </nav>
             <div className="resume">
-                <iframe title="Resume" src="public\resumemay2020.pdf" width="500" height="600"></iframe>
+            <iframe src="https://drive.google.com/file/d/1C_go3lJxisGfqw7Rn1J4ahrreMmrpzKh/preview" width="640" height="600"></iframe>
             </div>
+            <Document file="public/resumemay2020.pdf">
+                <Page pageNumber={1} />
+            </Document>
+            
         </div>
     )
 };
+
 
 export default Resume;
