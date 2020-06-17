336b9d3b1d675504470d857992b0833d7285743c
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2020-06-16 21:48:28 -0500

    Add projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
     
     return (
         <div>
-            <h1>Projects page</h1>
+            <nav className="top-nav">
+                <div className="top-nav-items">
+                    <Link to="/">
+                        <button className="current">about</button>
+                    </Link>
+                    <Link to="/projects">
+                        <button>projects</button>
+                    </Link>
+                    <Link to="/resume">
+                        <button>resume</button>
+                    </Link>
+                    <Link to="/contact">
+                        <button>contact</button>
+                    </Link>
+                </div>
+            </nav>
+            <div className="projects-box">
+                <div className="project">
+                    <a href="https://careerkarma.com/discussions/projects/food-journal-519/">
+                        <img src="https://i.imgur.com/Khdt6cM.png" alt="oh.. hello" />
+                    </a>
+                    <div className="project-text">
+                        <a href="https://careerkarma.com/discussions/projects/food-journal-519/">Food Journal</a>
+                        <p>An alternative to typical calorie-tracking apps that focuses on logging meals with pictures.  Type the calories and take a picture, and the calorie total of the day will update.</p>
+                        <div className="labels">
+                            <div>javascript</div>
+                            <div>html</div>
+                            <div>css</div>
+                            <div>webrtc api</div>
+                            <div>firebase</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="project">
+                    <p>IM A PROJECT TOO</p>
+                </div>
+            </div>
         </div>
     )
 };
