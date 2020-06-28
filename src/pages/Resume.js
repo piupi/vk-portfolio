d4dec8502a4ead28b6ea2e8072f1db04a57c644e
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2020-06-28 11:18:57 -0500

    Display different resume in mobile browsers

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -22,6 +22,10 @@ const Resume = () => {
             </nav>
             <div className="resume">
             <iframe title="Resume" src="https://drive.google.com/file/d/1C_go3lJxisGfqw7Rn1J4ahrreMmrpzKh/preview" width="750" height="600"></iframe>
+            <object data='https://drive.google.com/file/d/1C_go3lJxisGfqw7Rn1J4ahrreMmrpzKh/preview'>
+                <p>Oops! Your browser doesn't support PDFs!</p>
+                <p><a href="https://pdfhost.io/v/bV5p8xyXa_Vanessa_K_Resume_May2020pdf.pdf">Click to view on PDFHost instead</a></p>
+            </object>
             </div>
         </div>
     )
