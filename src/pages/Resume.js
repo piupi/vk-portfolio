4f9c7fd179f5524cae6fbb21f34825d89ca8a00f
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2020-06-28 11:32:13 -0500

    Add background for PDF error

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -23,8 +23,10 @@ const Resume = () => {
             <div className="resume">
             <iframe title="Resume" src="https://drive.google.com/file/d/1C_go3lJxisGfqw7Rn1J4ahrreMmrpzKh/preview" width="750" height="600"></iframe>
             <object data='https://drive.google.com/file/d/1C_go3lJxisGfqw7Rn1J4ahrreMmrpzKh/preview'>
+                <div id="pdf-error">
                 <p>Oops! Your browser doesn't support PDFs!</p>
-                <p><a href="https://pdfhost.io/v/bV5p8xyXa_Vanessa_K_Resume_May2020pdf.pdf">Click to view on PDFHost instead</a></p>
+                <p><a href="https://pdfhost.io/v/bV5p8xyXa_Vanessa_K_Resume_May2020pdf.pdf">Click to view on PDF Host instead</a></p>
+                </div>
             </object>
             </div>
         </div>
